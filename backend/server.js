d27ae277adeaeb227c2af7780964dbedfc2d74c5
atizaz786 <atizazhashmi786@gmail.com>
2024-03-04 13:00:21 +0000

    feat: Added relevant code for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -10,18 +11,20 @@ import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
 // Middlewares to parse the incoming request body
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 // Middlewares to parse the incoming cookies
 app.use(cookieParser());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
